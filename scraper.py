a56168ec683bd1a8fc37756faab4072b0fae6c02
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2024-10-07 01:28:46 +0800

    Updated inline documentation

--- scraper.py
+++ scraper.py
@@ -2,7 +2,13 @@ from datetime import datetime as dt
 from bs4 import BeautifulSoup as bs
 import requests, re, os 
 
-# get each individual article's sections
+# Get each article's section categories
+# @param sectionContainers 
+# - The list of HTML elements holding the sections for each article.
+# @param artNum 
+# - The index of the article in the list of sectionContainers.
+#
+# @return The section(s) associated with the given article.
 def getSections(sectionContainers, artNum):
     sectionList = sectionContainers[artNum].find_all(
         'a', rel='category tag'
@@ -10,7 +16,17 @@ def getSections(sectionContainers, artNum):
     if (len(sectionList) == 1): return sectionList[0].text
     return sectionList[0].text + " & " + sectionList[1].text 
 
-# display collected titles and page links
+# Print the article's title, link, and section category 
+# via terminal and text file output
+# 
+# @param titles 
+# - The list of HTML elements containing the titles of the articles.
+# @param links 
+# - The list of HTML elements containing the links to the articles.
+# @param sectionContainers 
+# - The list of HTML containers holding the sections for each article.
+#
+# @return none
 def dispArticleInfo(titles, links, sectionContainers):
     for i in range(len(titles)): 
         sections = getSections(sectionContainers, i)  
@@ -23,7 +39,7 @@ def dispArticleInfo(titles, links, sectionContainers):
         file.writelines(f"\nLink: {links[i].get('href')}\n")
 
 year = '2009'
-# prompt user to input a year
+# Prompt the user to input a valid year (no earlier than 2010) until a valid input is provided.
 while (int(year) <= 2009 or int(year) > dt.now().year):
     year = input("Input a year no earlier than 2010: ") 
     if (int(year) <= 2009 or int(year) > dt.now().year):
@@ -35,10 +51,11 @@ req  = requests.get(URL)
 soup = bs(req.text, 'html.parser')
 file = open(year+"-articles.txt", "w")
 
-# find the max page count per year
+# Find the maximum number of pages of articles for the given year 
 pageCount = soup.find_all('a', class_='page-numbers')
 pageCount = int(pageCount[-2].text)+1
 
+# 
 for i in range(1, pageCount):
     URL  = 'https://thelasallian.com/'+year+'/page/'+str(i)+'/'
     req  = requests.get(URL)
